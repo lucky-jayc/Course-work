207a62461267354c2bc0e9c90967074c7cbb8cb2
tech-man <jayclucky@gmail.com>
2026-09-11 13:49:13 +0300

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,15 @@
+Class Library{
+System.out.println("This is my library");
+}
+class Book extends Library {
+void borrow(){
+System.out.println("You borrowed a book");
+public class Library{
+public static void main(String[] args){
+Library math;
+math = new borrow();
+math.borrow();
+}
+}
+}
+}
